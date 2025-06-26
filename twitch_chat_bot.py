e19f51a3726f862e9b6a166d11e7f4b39e50ebe6
natukin1978 <natukin1978@hotmail.com>
2025-06-26 17:42:56 +0900

    If an error code exists, a response is returned.

--- twitch_chat_bot.py
+++ twitch_chat_bot.py
@@ -70,6 +70,9 @@ async def main():
 
     # 注意. 判定フラグを削除するため、受信ハンドラでこの関数を複数回呼んではいけない
     def is_needs_response(json_data: dict[str, any]) -> bool:
+        if json_data["errorCode"]:
+            return True
+
         request_id = json_data["request"]["id"]
         if fs_response.should_skip(request_id):
             # 同じIDで頻繁にレス返すのを抑止
