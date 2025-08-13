033fe3071be28f13d1f0c9365f408dc586af74f0
natukin1978 <natukin1978@hotmail.com>
2025-08-13 23:28:47 +0900

    refactoring.

--- keywords_helper.py
+++ keywords_helper.py
@@ -1,12 +1,16 @@
 import global_value as g
 
 
+def has_keywords(message: str, keywords: list[str]) -> bool:
+    return next(filter(lambda v: v in message, keywords), None)
+
+
 def has_keywords_response(message: str) -> bool:
     conf_fa = g.config["fuyukaApi"]
     if not conf_fa:
         return False
     response_keywords = conf_fa["responseKeywords"]
-    return next(filter(lambda v: v in message, response_keywords), None)
+    return has_keywords(message, response_keywords)
 
 
 def has_keywords_exclusion(message: str) -> bool:
@@ -14,4 +18,4 @@ def has_keywords_exclusion(message: str) -> bool:
     if not conf_fa:
         return False
     exclusion_keywords = conf_fa["exclusionKeywords"]
-    return next(filter(lambda v: v in message, exclusion_keywords), None)
+    return has_keywords(message, exclusion_keywords)
