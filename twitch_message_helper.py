30807a02af0c4d80f339135ae674213a43f072a4
natukin1978 <natukin1978@hotmail.com>
2025-09-01 01:32:46 +0900

    Separated create_message_json into two functions.

--- twitch_message_helper.py
+++ twitch_message_helper.py
@@ -5,23 +5,28 @@ import twitchio
 from one_comme_users import OneCommeUsers
 
 
-def create_message_json(msg: twitchio.Message = None) -> dict[str, any]:
+def create_message_json(
+    id: str, displayName: str, isFirst: bool, content: str
+) -> dict[str, any]:
     localtime = datetime.datetime.now()
     localtime_iso_8601 = localtime.isoformat()
     json_data = {
         "dateTime": localtime_iso_8601,
-        "id": None,
-        "displayName": None,
+        "id": id,
+        "displayName": displayName,
         "nickname": None,
-        "content": None,  # 関数外で設定してね
-        "isFirst": False,
+        "content": content,
+        "isFirst": isFirst,
         "isFirstOnStream": None,  # すぐ下で設定する
         "noisy": False,
         "additionalRequests": None,
     }
-    if msg:
-        json_data["id"] = msg.author.name
-        json_data["displayName"] = msg.author.display_name
-        json_data["isFirst"] = msg.first
     OneCommeUsers.update_message_json(json_data)
     return json_data
+
+
+def create_message_json(msg: twitchio.Message, content: str) -> dict[str, any]:
+    id = msg.author.name
+    displayName = msg.author.display_name
+    isFirst = msg.first
+    return create_message_json(id, displayName, isFirst, content)
