72a5c77702a46fc7e41f0334ae78604178635d82
natukin1978 <natukin1978@hotmail.com>
2026-03-29 22:50:43 +0900

    Ported the Quickstart sample bot.

--- twitch_bot.py
+++ twitch_bot.py
@@ -3,11 +3,17 @@ import datetime
 import json
 import logging
 import re
+from typing import TYPE_CHECKING
 
+import asqlite
 import aiohttp
 import twitchio
 from bs4 import BeautifulSoup
+from twitchio import eventsub
 from twitchio.ext import commands
+
+if TYPE_CHECKING:
+    import sqlite3
 
 import global_value as g
 from emote_helper import add_emotes, remove_emote
@@ -25,142 +31,206 @@ from twitch_message_helper import (
 logger = logging.getLogger(__name__)
 
 
-class TwitchBot(commands.Bot):
-    def __init__(self):
-        self.login_channel = g.config["twitch"]["loginChannel"]
+class TwitchBot(commands.AutoBot):
+    def __init__(
+        self, *, token_database: asqlite.Pool, subs: list[eventsub.SubscriptionPayload]
+    ) -> None:
+        self.token_database = token_database
+
+        ctw = g.config["twitch"]
         super().__init__(
-            token=g.config["twitch"]["accessToken"],
+            client_id=ctw["clientId"],
+            client_secret=ctw["clientSecret"],
+            bot_id=ctw["botId"],
+            owner_id=ctw["ownerId"],
             prefix="!",
-            initial_channels=[self.login_channel],
+            subscriptions=subs,
+            force_subscribe=True,
         )
 
-    @staticmethod
-    def find_url(text: str) -> str:
-        # 正規表現パターン
-        # このパターンは、httpやhttpsプロトコルを含むURLを検索します。
-        # 特に、ドメイン名やサブドメイン、ポート番号などを考慮しています。
-        RE_URL = r"https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
-        urls = re.findall(RE_URL, text)
-        if urls:
-            return urls[0]  # 最初のURLを返す
-        return ""
+    async def setup_hook(self) -> None:
+        # Add our component which contains our commands...
+        await self.add_component(MyComponent(self))
 
-    @staticmethod
-    async def web_scraping(url: str, renderType: str) -> str:
-        param = {
-            "url": url,
-            "renderType": renderType,
-        }
-        API_URL = (
-            "http://PhantomJScloud.com/api/browser/v2/"
-            + g.config["phantomJsCloud"]["apiKey"]
-            + "/"
+    async def event_oauth_authorized(
+        self, payload: twitchio.authentication.UserTokenPayload
+    ) -> None:
+        await self.add_token(payload.access_token, payload.refresh_token)
+
+        if not payload.user_id:
+            return
+
+        if payload.user_id == self.bot_id:
+            # We usually don't want subscribe to events on the bots channel...
+            return
+
+        # A list of subscriptions we would like to make to the newly authorized channel...
+        subs: list[eventsub.SubscriptionPayload] = [
+            eventsub.ChatMessageSubscription(
+                broadcaster_user_id=payload.user_id, user_id=self.bot_id
+            ),
+        ]
+
+        resp: twitchio.MultiSubscribePayload = await self.multi_subscribe(subs)
+        if resp.errors:
+            logger.warning(
+                "Failed to subscribe to: %r, for user: %s", resp.errors, payload.user_id
+            )
+
+    async def add_token(
+        self, token: str, refresh: str
+    ) -> twitchio.authentication.ValidateTokenPayload:
+        # Make sure to call super() as it will add the tokens interally and return us some data...
+        resp: twitchio.authentication.ValidateTokenPayload = await super().add_token(
+            token, refresh
         )
-        async with aiohttp.ClientSession() as session:
-            async with session.post(API_URL, data=json.dumps(param)) as response:
-                return await response.text()
 
-    @staticmethod
-    def get_all_contents(html_content: str, target_selector: str) -> list:
-        soup = BeautifulSoup(html_content, "html.parser")
-        elem = soup.select_one(target_selector)
-        elem_strings = elem.stripped_strings
-        return [elem_string for elem_string in elem_strings]
+        # Store our tokens in a simple SQLite Database when they are authorized...
+        query = """
+        INSERT INTO tokens (user_id, token, refresh)
+        VALUES (?, ?, ?)
+        ON CONFLICT(user_id)
+        DO UPDATE SET
+            token = excluded.token,
+            refresh = excluded.refresh;
+        """
 
-    async def send_message(self, json_data: dict[str, any], answer_level: int):
-        answer_length = g.config["fuyukaApi"]["answerLength"]["default"]
+        async with self.token_database.acquire() as connection:
+            await connection.execute(query, (resp.user_id, token, refresh))
 
-        if g.config["phantomJsCloud"]["apiKey"]:
-            content = json_data["content"]
-            url = TwitchBot.find_url(content)
-            if url:
-                logger.info("web_scraping: " + url)
-                if "www.twitch.tv" in url:
-                    content = await TwitchBot.web_scraping(url, "html")
-                    contents_list = TwitchBot.get_all_contents(
-                        content, "[class*='channel-info-content']"
-                    )
-                    content = "\n".join(contents_list)
-                else:
-                    content = await TwitchBot.web_scraping(url, "plainText")
+        logger.info("Added token to the database for user: %s", resp.user_id)
+        return resp
 
-                json_data["content"] = g.WEB_SCRAPING_PROMPT + "\n" + content
-                answer_length = g.config["fuyukaApi"]["answerLength"]["webScraping"]
-                answer_level = 100  # 常に回答してください
+    async def event_ready(self) -> None:
+        logger.info("Successfully logged in as: %s", self.bot_id)
 
-        needs_response = is_hit_by_message_json(answer_level, json_data)
-        if not needs_response:
-            answer_length = 0
-        OneCommeUsers.update_additional_requests(json_data, answer_length)
-        await Fuyuka.send_message_by_json_with_buf(json_data, needs_response)
+    def get_component(self):
+        return self.get_component("MyComponent")
 
-    async def do_time_signal(self, interval_minutes: int, message: str):
-        fs_time_signal = FunctionSkipper(45)
-        while True:
-            if fs_time_signal.should_skip(""):
-                # 念のため、頻繁に処理されないようにする
-                await asyncio.sleep(1)
+
+class MyComponent(commands.Component):
+    # An example of a Component with some simple commands and listeners
+    # You can use Components within modules for a more organized codebase and hot-reloading.
+
+    def __init__(self, bot: TwitchBot) -> None:
+        # Passing args is not required...
+        # We pass bot here as an example...
+        self.bot = bot
+
+    # An example of listening to an event
+    # We use a listener in our Component to display the messages received.
+    @commands.Component.listener()
+    async def event_message(self, payload: twitchio.ChatMessage) -> None:
+        print(f"[{payload.broadcaster.name}] - {payload.chatter.name}: {payload.text}")
+
+    @commands.command()
+    async def hi(self, ctx: commands.Context) -> None:
+        """Command that replies to the invoker with Hi <name>!
+
+        !hi
+        """
+        await ctx.reply(f"Hi {ctx.chatter}!")
+
+    @commands.command()
+    async def say(self, ctx: commands.Context, *, message: str) -> None:
+        """Command which repeats what the invoker sends.
+
+        !say <message>
+        """
+        await ctx.send(message)
+
+    @commands.command()
+    async def add(self, ctx: commands.Context, left: int, right: int) -> None:
+        """Command which adds to integers together.
+
+        !add <number> <number>
+        """
+        await ctx.reply(f"{left} + {right} = {left + right}")
+
+    @commands.command()
+    async def choice(self, ctx: commands.Context, *choices: str) -> None:
+        """Command which takes in an arbitrary amount of choices and randomly chooses one.
+
+        !choice <choice_1> <choice_2> <choice_3> ...
+        """
+        await ctx.reply(
+            f"You provided {len(choices)} choices, I choose: {random.choice(choices)}"
+        )
+
+    @commands.command(aliases=["thanks", "thank"])
+    async def give(
+        self,
+        ctx: commands.Context,
+        user: twitchio.User,
+        amount: int,
+        *,
+        message: str | None = None,
+    ) -> None:
+        """A more advanced example of a command which has makes use of the powerful argument parsing, argument converters and
+        aliases.
+
+        The first argument will be attempted to be converted to a User.
+        The second argument will be converted to an integer if possible.
+        The third argument is optional and will consume the reast of the message.
+
+        !give <@user|user_name> <number> [message]
+        !thank <@user|user_name> <number> [message]
+        !thanks <@user|user_name> <number> [message]
+        """
+        msg = f"with message: {message}" if message else ""
+        await ctx.send(
+            f"{ctx.chatter.mention} gave {amount} thanks to {user.mention} {msg}"
+        )
+
+    @commands.group(invoke_fallback=True)
+    async def socials(self, ctx: commands.Context) -> None:
+        """Group command for our social links.
+
+        !socials
+        """
+        await ctx.send("discord.gg/..., youtube.com/..., twitch.tv/...")
+
+    @socials.command(name="discord")
+    async def socials_discord(self, ctx: commands.Context) -> None:
+        """Sub command of socials that sends only our discord invite.
+
+        !socials discord
+        """
+        await ctx.send("discord.gg/...")
+
+
+async def setup_database(
+    db: asqlite.Pool,
+) -> tuple[list[tuple[str, str]], list[eventsub.SubscriptionPayload]]:
+    # Create our token table, if it doesn't exist..
+    # You should add the created files to .gitignore or potentially store them somewhere safer
+    # This is just for example purposes...
+
+    query = """CREATE TABLE IF NOT EXISTS tokens(user_id TEXT PRIMARY KEY, token TEXT NOT NULL, refresh TEXT NOT NULL)"""
+    async with db.acquire() as connection:
+        await connection.execute(query)
+
+        # Fetch any existing tokens...
+        rows: list[sqlite3.Row] = await connection.fetchall("""SELECT * from tokens""")
+
+        tokens: list[tuple[str, str]] = []
+        subs: list[eventsub.SubscriptionPayload] = []
+
+        ctw = g.config["twitch"]
+        bot_id = ctw["botId"]
+
+        for row in rows:
+            tokens.append((row["token"], row["refresh"]))
+
+            if row["user_id"] == bot_id:
                 continue
 
-            now = datetime.datetime.now()
-            next_time = calculate_next_time(now, interval_minutes)
-            wait_seconds = (next_time - now).total_seconds()
-            await asyncio.sleep(wait_seconds)
+            subs.extend(
+                [
+                    eventsub.ChatMessageSubscription(
+                        broadcaster_user_id=row["user_id"], user_id=bot_id
+                    )
+                ]
+            )
 
-            id = g.config["twitch"]["loginChannel"]
-            display_name = g.talker_name
-            content = message.strip()
-            json_data = create_message_json(id, display_name, False, content)
-            answer_level = 100
-            await self.send_message(json_data, answer_level)
-
-    async def event_message(self, message: twitchio.Message):
-        if message.echo:
-            return
-
-        id = message.author.name
-        if id in g.set_exclude_id:
-            # 無視するID
-            return
-
-        if message.content.startswith("!"):
-            await self.handle_commands(message)
-            return
-
-        emotes = []
-        add_emotes(emotes, message)
-        text = remove_emote(message.content, emotes)
-        if not text:
-            return
-
-        if has_keywords_exclusion(text):
-            # 除外キーワードは取り込まない
-            return
-
-        json_data = create_message_json_from_twitchio_message(message, text)
-        answer_level = 0
-        if has_keywords_response(text):
-            answer_level = 100  # 常に回答してください
-        else:
-            answer_level = g.config["fuyukaApi"]["answerLevel"]
-        await self.send_message(json_data, answer_level)
-
-    @staticmethod
-    def get_cmd_value(content: str) -> str:
-        pattern = r"^![^ ]+ (.*?)$"
-        match = re.search(pattern, content)
-        if not match:
-            logger.debug("Not match: " + content)
-            return ""
-
-        return match.group(1)
-
-    @commands.command(name="ai")
-    async def cmd_ai(self, ctx: commands.Context):
-        text = TwitchBot.get_cmd_value(ctx.message.content)
-
-        json_data = create_message_json_from_twitchio_message(ctx.message, text)
-        answer_length = g.config["fuyukaApi"]["answerLength"]["aiCmd"]
-        needs_response = True
-        OneCommeUsers.update_additional_requests(json_data, answer_length)
-        await Fuyuka.send_message_by_json_with_buf(json_data, needs_response)
+    return tokens, subs
