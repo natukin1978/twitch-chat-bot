4ac0a737aa6468dbee3c25678594a2339760c860
natukin1978 <natukin1978@hotmail.com>
2026-04-02 18:48:27 +0900

    A message will be output if the connection to the server is successful.

--- websocket_helper.py
+++ websocket_helper.py
@@ -25,6 +25,7 @@ async def websocket_listen_forever(
             async with websockets.connect(websocket_uri) as ws:
                 if handle_set_websocket:
                     handle_set_websocket(ws)
+                print(f"接続成功しました。{websocket_uri}")
                 while True:
                     # listener loop
                     try:
